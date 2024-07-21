87ec38aa5531279fe90c20f655b3e32e66d36358
Ahmed kassab <agameover25@gmail.com>
2024-07-21 13:00:40 +0300

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.main {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.main to javafx.fxml;
+    exports org.example.main;
+}
