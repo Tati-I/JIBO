03785473eb4012722367e5a5c47996498399a4e5
Ahmed kassab <agameover25@gmail.com>
2024-07-21 15:38:31 +0300

    تعديل واجهة

--- src/main/java/org/example/main/HelloApplication.java
+++ src/main/java/org/example/main/HelloApplication.java
@@ -3,6 +3,7 @@ package org.example.main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,12 +11,17 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("my name qewqeqwe");
+        Pane root = new Pane();
+        Scene scene = new Scene(root,650,700);
+
+
+
+
+        stage.setTitle("Hello World");
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
