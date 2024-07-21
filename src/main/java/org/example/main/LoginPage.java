13139df502471d6ad8025b3d06b3d206771ade7b
Ahmed kassab <agameover25@gmail.com>
2024-07-21 16:38:16 +0300

    re-design

--- src/main/java/org/example/main/LoginPage.java
+++ src/main/java/org/example/main/LoginPage.java
@@ -1,7 +1,11 @@
 package org.example.main;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -9,13 +13,33 @@ import java.io.IOException;
 public class LoginPage extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Pane root = new Pane();
-        Scene scene = new Scene(root,650,700);
+        AnchorPane root = new AnchorPane();
+        Scene scene = new Scene(root,650,700, Color.BLUEVIOLET);
+        Rectangle rectangle = new Rectangle();
+        rectangle.setHeight(435);
+        rectangle.setWidth(485);
+        rectangle.setX(82);
+        rectangle.setY(133);
+        rectangle.setFill(Color.WHITE);
+        rectangle.setArcHeight(25);
+        rectangle.setArcWidth(25);
 
 
 
 
-        stage.setTitle("Hello World");
+        Label WelcomeMsg = new Label("مرحبًا بك في تطبيق JIBO");
+        WelcomeMsg.setLayoutX(207);
+        WelcomeMsg.setLayoutY(161);
+        WelcomeMsg.setStyle("-fx-text-fill: #000000; -fx-font-weight: bold; -fx-font-size: 22");
+
+
+
+        root.getChildren().addAll(rectangle, WelcomeMsg);
+
+
+
+
+        stage.setTitle("Jibo");
         stage.setScene(scene);
         stage.show();
     }
