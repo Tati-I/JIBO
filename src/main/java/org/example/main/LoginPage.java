0add5845a06823603e0a6bd6b1e0d46de5da612e
Ahmed kassab <agameover25@gmail.com>
2024-07-23 23:18:34 +0300

    login finish

--- src/main/java/org/example/main/LoginPage.java
+++ src/main/java/org/example/main/LoginPage.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -20,20 +21,20 @@ public class LoginPage extends Application {
     public void start(Stage stage) throws IOException {
         AnchorPane root = new AnchorPane();
 
-        // تحميل الصورة
+
         Image background = new Image(getClass().getResource("/Pictures/background.png").toExternalForm());
         ImageView imageView = new ImageView(background);
         imageView.setFitWidth(700);
         imageView.setFitHeight(720);
 
-        // إنشاء لوحة
+
         Pane pane = new Pane();
         pane.setStyle("-fx-background-color: white; -fx-background-radius: 27px;");
         pane.setPrefSize(525, 470);
         pane.setLayoutX(88);
         pane.setLayoutY(144);
 
-        // إنشاء رسالة الترحيب
+
         Label WelcomeMsg = new Label("مرحبًا بك في تطبيق JIBO");
         WelcomeMsg.setLayoutX(140);
         WelcomeMsg.setLayoutY(30);
@@ -42,7 +43,7 @@ public class LoginPage extends Application {
         Label bioMsg = new Label("حِرفيُّكَ الخاص في جيبك ");
         bioMsg.setLayoutX(175);
         bioMsg.setLayoutY(60);
-        bioMsg.setStyle("-fx-text-fill: #565454; -fx-font-size: 17");
+        bioMsg.setStyle("-fx-text-fill: #6e6d6d; -fx-font-size: 17");
 
 
         Rectangle loginRect = new Rectangle();
@@ -51,23 +52,80 @@ public class LoginPage extends Application {
         loginRect.setX(30);
         loginRect.setY(100);
         loginRect.setFill(Color.web("F1F2F4FF"));
-        loginRect.setArcHeight(13);
-        loginRect.setArcWidth(13);
+        loginRect.setArcHeight(25);
+        loginRect.setArcWidth(25);
 
         Button loginButton = new Button("تسجيل الدخول");
         loginButton.setLayoutX(35);
         loginButton.setLayoutY(103.5);
         loginButton.setPrefSize(225, 36);
-        loginButton.setStyle("-fx-border-color: #e3e3e3; -fx-border-radius: 5px; -fx-text-fill: #000000; -fx-font-size: 15; -fx-background-color: white; -fx-background-radius: 5px; -fx-cursor: hand;");
+        loginButton.setStyle("-fx-border-color: #e3e3e3; -fx-border-radius: 10px; -fx-text-fill: #000000; -fx-font-size: 15; -fx-background-color: white; -fx-background-radius: 10px; -fx-cursor: hand;");
 
         Button signUpButton = new Button("إنشاء حساب");
         signUpButton.setLayoutX(262.5);
         signUpButton.setLayoutY(103.5);
         signUpButton.setPrefSize(225, 36);
-        signUpButton.setStyle("-fx-text-fill: #000000; -fx-font-size: 15; -fx-background-color: #f1f2f4; -fx-background-radius: 5px; -fx-cursor: hand;");
+        signUpButton.setStyle("-fx-text-fill: #000000; -fx-font-size: 15; -fx-background-color: #f1f2f4; -fx-background-radius: 10px; -fx-cursor: hand;");
 
         TranslateTransition loginTransition = new TranslateTransition(Duration.millis(300), loginButton);
         TranslateTransition signUpTransition = new TranslateTransition(Duration.millis(300), signUpButton);
+
+
+        Label emailWord = new Label("البريد الإلكتروني");
+        emailWord.setLayoutX(390);
+        emailWord.setLayoutY(170);
+        emailWord.setStyle("-fx-text-fill: #000000; -fx-font-size: 18; -fx-font-weight: bold;");
+
+        TextField emailField = new TextField();
+        emailField.setPromptText("البريد الألكتروني");
+        emailField.setPrefSize(495, 40);
+        emailField.setLayoutX(15);
+        emailField.setLayoutY(210);
+        emailField.setStyle("-fx-background-radius: 10; -fx-border-radius: 10; -fx-border-color: #d0caca;");
+
+
+        Label passwordWord = new Label("كلمة المرور");
+        passwordWord.setLayoutX(425);
+        passwordWord.setLayoutY(255);
+        passwordWord.setStyle("-fx-text-fill: #000000; -fx-font-size: 18; -fx-font-weight: bold;");
+
+
+        TextField passwordField = new TextField();
+        passwordField.setPromptText("كلمة المرور");
+        passwordField.setPrefSize(495, 40);
+        passwordField.setLayoutX(15);
+        passwordField.setLayoutY(290);
+        passwordField.setStyle("-fx-background-radius: 10; -fx-border-radius: 10; -fx-border-color: #D0CACAFF;");
+
+
+
+        Button Login = new Button("تسجيل الدخول");
+        Login.setLayoutX(15);
+        Login.setLayoutY(350);
+        Login.setPrefSize(495, 40);
+        Login.setStyle("-fx-background-radius: 10; -fx-border-radius: 10; -fx-background-color: #01012a; -fx-text-fill: white; -fx-font-size: 16;");
+
+
+        Label privacy = new Label("بالتسجيل، أنت توافق على الشروط والأحكام وسياسة الخصوصية");
+        privacy.setStyle("-fx-text-fill: #6e6d6d; -fx-font-size: 16");
+        privacy.setLayoutX(60);
+        privacy.setLayoutY(410);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         signUpButton.setOnAction(event -> {
             signUpTransition.setToX(-228);
@@ -75,8 +133,8 @@ public class LoginPage extends Application {
             loginTransition.setToX(225);
             loginTransition.play();
 
-            signUpButton.setStyle("-fx-border-color: #e3e3e3; -fx-border-radius: 5px; -fx-background-color: white; -fx-background-radius: 5px; -fx-cursor: hand; -fx-text-fill: #000000; -fx-font-size: 15;");
-            loginButton.setStyle("-fx-background-color: #f1f2f4; -fx-background-radius: 5px; -fx-cursor: hand; -fx-text-fill: #000000; -fx-font-size: 15;");
+            signUpButton.setStyle("-fx-border-color: #e3e3e3; -fx-border-radius: 10px; -fx-background-color: white; -fx-background-radius: 10px; -fx-cursor: hand; -fx-text-fill: #000000; -fx-font-size: 15;");
+            loginButton.setStyle("-fx-background-color: #f1f2f4; -fx-background-radius: 10px;-fx-border-radius: 10px; -fx-cursor: hand; -fx-text-fill: #000000; -fx-font-size: 15;");
         });
 
         loginButton.setOnAction(event -> {
@@ -85,12 +143,41 @@ public class LoginPage extends Application {
             signUpTransition.setToX(0);
             signUpTransition.play();
 
-            loginButton.setStyle("-fx-border-color: #e3e3e3; -fx-border-radius: 5px; -fx-background-color: #fdfdfd; -fx-background-radius: 5px; -fx-cursor: hand; -fx-text-fill: #000000; -fx-font-size: 15;");
-            signUpButton.setStyle("-fx-text-fill: #000000; -fx-font-size: 15; -fx-background-color: #f1f2f4; -fx-background-radius: 5px; -fx-cursor: hand;");
+            loginButton.setStyle("-fx-border-color: #e3e3e3; -fx-border-radius: 10px; -fx-background-color: #fdfdfd; -fx-background-radius: 10px; -fx-cursor: hand; -fx-text-fill: #000000; -fx-font-size: 15;");
+            signUpButton.setStyle("-fx-text-fill: #000000; -fx-font-size: 15; -fx-background-color: #f1f2f4; -fx-background-radius: 10px;-fx-border-radius: 10px; -fx-cursor: hand;");
         });
 
 
-        pane.getChildren().addAll(WelcomeMsg, bioMsg, loginRect, loginButton, signUpButton);
+        emailField.setOnMousePressed(event -> {
+            passwordField.setStyle("-fx-background-radius: 10; -fx-border-radius: 10; -fx-border-color: #d0caca; -fx-cursor: hand;");
+
+            emailField.setStyle("-fx-background-radius: 10; -fx-border-radius: 10; -fx-border-color: #000000; -fx-cursor: hand;");
+        });
+        passwordField.setOnMousePressed(event ->{
+           emailField.setStyle("-fx-background-radius: 10; -fx-border-radius: 10; -fx-border-color: #d0caca;");
+
+            passwordField.setStyle("-fx-background-radius: 10; -fx-border-radius: 10; -fx-border-color: #000000; -fx-cursor: hand;");
+
+
+        });
+
+        Login.setOnMouseEntered(event ->{
+            Login.setStyle("-fx-background-radius: 10; -fx-border-radius: 10; -fx-background-color: #090942; -fx-text-fill: white; -fx-font-size: 16; -fx-cursor: hand;");
+
+
+        });
+        Login.setOnMouseExited(event ->{
+            Login.setStyle("-fx-background-radius: 10; -fx-border-radius: 10; -fx-background-color: #01012a; -fx-text-fill: white; -fx-font-size: 16;");
+
+
+        });
+
+
+
+
+        pane.getChildren().addAll(WelcomeMsg, bioMsg, loginRect, loginButton, signUpButton,emailWord, emailField,passwordWord, passwordField,Login,privacy);
+
+
 
 
         root.getChildren().addAll(imageView, pane);
@@ -98,6 +185,16 @@ public class LoginPage extends Application {
         Scene scene = new Scene(root, 700, 720);
 
 
+        scene.getStylesheets().add((getClass().getResource("/styles/style.css").toExternalForm()));
+
+
+
+        scene.setOnMousePressed(event ->{
+            emailField.setStyle("-fx-background-radius: 10; -fx-border-radius: 10; -fx-border-color: #d0caca;");
+
+            passwordField.setStyle("-fx-background-radius: 10; -fx-border-radius: 10; -fx-border-color: #d0caca; -fx-cursor: hand;");
+
+        });
         stage.setTitle("Jibo");
         stage.setResizable(false);
         stage.setScene(scene);
