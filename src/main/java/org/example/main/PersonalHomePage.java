4fe8c3b69c9319d521f397043302fafa07a79bfb
Ahmed kassab <agameover25@gmail.com>
2024-08-05 22:20:59 +0300

    PersonalHomePage

--- src/main/java/org/example/main/PersonalHomePage.java
+++ src/main/java/org/example/main/PersonalHomePage.java
@@ -1,82 +1,88 @@
 package org.example.main;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.util.Objects;
-
 public class PersonalHomePage extends Application {
+
     public void start(Stage primaryStage) {
-        Pane root = new Pane();
-        Scene scene = new Scene(root, 1200, 780);
 
-        scene.getStylesheets().add(getClass().getResource("/styles/style.css").toExternalForm());
-
-
-        Image settingIcon = new Image(Objects.requireNonNull(getClass().getResource("/Pictures/settings.png")).toExternalForm());
-        ImageView settingIconView = new ImageView(settingIcon);
-        settingIconView.setFitWidth(17);
-        settingIconView.setFitHeight(17);
-        settingIconView.setLayoutX(1);
-        settingIconView.setLayoutY(114);
-
-        Image aboutIcon = new Image(Objects.requireNonNull(getClass().getResource("/Pictures/about.png")).toExternalForm());
-        ImageView aboutIconView = new ImageView(aboutIcon);
-        aboutIconView.setFitWidth(17);
-        aboutIconView.setFitHeight(17);
-        aboutIconView.setLayoutX(1);
-        aboutIconView.setLayoutY(165);
-
-        Image profileIcon = new Image(Objects.requireNonNull(getClass().getResource("/Pictures/profile.png")).toExternalForm());
-        ImageView profileIconView = new ImageView(profileIcon);
-        profileIconView.setFitWidth(18);
-        profileIconView.setFitHeight(18);
-        profileIconView.setLayoutX(1);
-        profileIconView.setLayoutY(11);
-
-        Image logoutIcon = new Image(Objects.requireNonNull(getClass().getResource("/Pictures/logout.png")).toExternalForm());
-        ImageView logoutIconView = new ImageView(logoutIcon);
-        logoutIconView.setFitWidth(18);
-        logoutIconView.setFitHeight(18);
-        logoutIconView.setLayoutX(1);
-        logoutIconView.setLayoutY(63);
-
-        VBox leftMenu = new VBox();
-        leftMenu.getStyleClass().add("left-menu");
-        leftMenu.setSpacing(10);
-        Button settingsBtn = new Button("Settings");
-        Button aboutBtn = new Button("About");
-        Button profileBtn = new Button("Profile");
-        Button logoutBtn = new Button("Logout");
+        Pane rightSideBar = new Pane();
+        rightSideBar.setPrefWidth(275);
+        rightSideBar.setPrefHeight(784);
+        rightSideBar.setLayoutX(927);
+        rightSideBar.setLayoutY(-1);
+        rightSideBar.setStyle("-fx-background-color: #4e79de; -fx-border-color: #ffffff; -fx-border-width: 1px;");
 
 
+        Label logo = new Label("LOGO");
+        logo.setMinSize(100, 100);
+        logo.setStyle("-fx-background-color: white; -fx-alignment: center;");
+        logo.setLayoutX(87.5);
+        logo.setLayoutY(50);
+
+
+        Button homeBtn = new Button("الصفحة الرئيسية");
+        homeBtn.setPrefSize(200,30);
+        homeBtn.setLayoutX(37.5);
+        homeBtn.setLayoutY(200);
+
+
+        Button requestServiceBtn = new Button("طلب خدمة");
+        requestServiceBtn.setPrefSize(200,30);
+        requestServiceBtn.setLayoutX(37.5);
+        requestServiceBtn.setLayoutY(250);
+
+
+        Button myServicesBtn = new Button("خدماتي");
+        myServicesBtn.setPrefSize(200,30);
+        myServicesBtn.setLayoutX(37.5);
+        myServicesBtn.setLayoutY(300);
+
+
+        Button profileBtn = new Button("ملفي الشخصي");
+        profileBtn.setPrefSize(200,30);
+        profileBtn.setLayoutX(37.5);
+        profileBtn.setLayoutY(350);
+
+
+        Button settingsBtn = new Button("إعدادات");
+        settingsBtn.setPrefSize(200,30);
+        settingsBtn.setLayoutX(37.5);
+        settingsBtn.setLayoutY(400);
+
+
+        Button logoutBtn = new Button("تسجيل الخروج");
+        logoutBtn.setPrefSize(100,30);
+        logoutBtn.setLayoutX(137.5);
+        logoutBtn.setLayoutY(700);
+
+        rightSideBar.getChildren().addAll(logo, homeBtn, requestServiceBtn, myServicesBtn, profileBtn, settingsBtn, logoutBtn);
 
 
 
-        leftMenu.getChildren().addAll(profileBtn, logoutBtn, settingsBtn, aboutBtn);
 
 
+        Pane mainLayout = new Pane(rightSideBar);
 
-
+        Scene scene = new Scene(mainLayout, 1200, 780, Color.WHITE);
+        primaryStage.setTitle("Jibo");
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Personal Home Page");
         primaryStage.setResizable(false);
         primaryStage.show();
-        root.getChildren().addAll(leftMenu,settingIconView,aboutIconView,profileIconView,logoutIconView);
-
-
-
-
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 }
