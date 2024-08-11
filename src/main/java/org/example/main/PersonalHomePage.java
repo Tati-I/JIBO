93be2a0d8f05e80a4065c102078b7a327cee9264
Ahmed kassab <agameover25@gmail.com>
2024-08-11 14:57:25 +0300

    PersonalHomePage

--- src/main/java/org/example/main/PersonalHomePage.java
+++ src/main/java/org/example/main/PersonalHomePage.java
@@ -35,13 +35,21 @@ public class PersonalHomePage extends Application {
         requestButton.setLayoutY(y);
         return requestButton;
     }
+    public static Button rightSideBarButton(String name,double x, double y) {
+        Button rightSideBarButton = new Button(name);
+        rightSideBarButton.setId("rightSideBarButton");
+        rightSideBarButton.setPrefSize(200,30);
+        rightSideBarButton.setLayoutX(x);
+        rightSideBarButton.setLayoutY(y);
+        return rightSideBarButton;
+    }
 
     public static Pane servicesPane(String title, String price, double x, double y) {
         Pane pane = new Pane();
         pane.setPrefSize(400, 110);
         pane.setLayoutX(x);
         pane.setLayoutY(y);
-        pane.setStyle("-fx-background-color: #4e79de; -fx-border-color: #4e79de;  -fx-border-radius: 10px; -fx-background-radius: 10");
+        pane.setStyle("-fx-background-color: #ffffff; -fx-border-color: #d2d2d2;  -fx-border-radius: 10px; -fx-background-radius: 10");
 
         Label titleLabel = new Label(title);
         titleLabel.setStyle("-fx-text-fill: #000000;");
@@ -52,6 +60,9 @@ public class PersonalHomePage extends Application {
         priceLabel.setStyle("-fx-text-fill: #000000;");
         priceLabel.setLayoutX(20);  // Adjusted positions
         priceLabel.setLayoutY(50);
+
+
+
 
 
 
@@ -68,7 +79,10 @@ public class PersonalHomePage extends Application {
         Image electric = new Image(getClass().getResource("/Pictures/Electric.jpeg").toExternalForm());
         Image ironmen = new Image(getClass().getResource("/Pictures/ironmen.jpeg").toExternalForm());
         Image water = new Image(getClass().getResource("/Pictures/watermen.jpeg").toExternalForm());
-        Image woodmen  = new Image(getClass().getResource("/Pictures/wood.jpeg").toExternalForm());
+        Image woodmen  = new Image(getClass().getResource("/Pictures/wood.png").toExternalForm());
+        Image logo1 = new Image(getClass().getResource("/Pictures/logo1.png").toExternalForm());
+        Image logo2 = new Image(getClass().getResource("/Pictures/logo2.png").toExternalForm());
+        Image accountPhoto = new Image(getClass().getResource("/Pictures/accountPhoto.png").toExternalForm());
 
 
 
@@ -93,15 +107,31 @@ public class PersonalHomePage extends Application {
         ImageView woodmenImageView = new ImageView(woodmen);
         woodmenImageView.setFitHeight(110);
         woodmenImageView.setFitWidth(110);
-        woodmenImageView.setLayoutX(651);
+        woodmenImageView.setLayoutX(645);
         woodmenImageView.setLayoutY(610);
+
+        ImageView logoImageView = new ImageView(logo1);
+        logoImageView.setFitHeight(250);
+        logoImageView.setFitWidth(250);
+        logoImageView.setLayoutX(12);
+        logoImageView.setLayoutY(-40);
+
+        ImageView logoImageView2 = new ImageView(logo2);
+        logoImageView2.setFitHeight(250);
+        logoImageView2.setFitWidth(250);
+        logoImageView2.setLayoutX(-50);
+        logoImageView2.setLayoutY(-60);
+
+        ImageView accountPhotoImageView = new ImageView(accountPhoto);
+        accountPhotoImageView.setFitHeight(32);
+        accountPhotoImageView.setFitWidth(32);
 
 
 
         Pane electricmenService = servicesPane("Electric Service", "123", 250, 200);
-        Pane woodmenService = servicesPane("Woodmen Service", "123", 250, 330);
+        Pane woodmenService = servicesPane("Watermen Service", "123", 250, 330);
         Pane ironmenService = servicesPane("Ironmen Service", "123", 250, 470);
-        Pane watermenService = servicesPane("Watermen Service", "123", 250, 610);
+        Pane watermenService = servicesPane("Woodmen Service", "123", 250, 610);
 
         Line line = new Line();
         line.setStartX(100);
@@ -121,10 +151,11 @@ public class PersonalHomePage extends Application {
         serviceSearch.setLayoutY(140);
         serviceSearch.setPrefSize(200,30);
 
-        Button goToProfile = new Button("صورة للملف الشخصي");
+        Button goToProfile = new Button("",accountPhotoImageView);
+        goToProfile.setStyle("-fx-background-color: #ffffff");
         goToProfile.setPrefSize(70,70);
-        goToProfile.setLayoutX(140);
-        goToProfile.setLayoutY(0);
+        goToProfile.setLayoutX(120);
+        goToProfile.setLayoutY(15);
 
         Button nowService = new Button("خدماتي الحالية");
         nowService.setPrefSize(100,30);
@@ -147,52 +178,30 @@ public class PersonalHomePage extends Application {
 
 
 
-        Label logo = new Label("LOGO");
-        logo.setMinSize(100, 100);
-        logo.setStyle("-fx-background-color: white; -fx-alignment: center;");
-        logo.setLayoutX(87.5);
-        logo.setLayoutY(50);
 
-        Label logo2 = new Label("LOGO");
-        logo2.setMinSize(70, 70);
-        logo2.setStyle("-fx-background-color: white; -fx-alignment: center;");
-        logo2.setLayoutX(50);
-        logo2.setLayoutY(0);
 
-        Button homeBtn = new Button("الصفحة الرئيسية");
-        homeBtn.setPrefSize(200, 30);
-        homeBtn.setLayoutX(37.5);
-        homeBtn.setLayoutY(200);
 
-        Button requestServiceBtn = new Button("طلب خدمة");
-        requestServiceBtn.setPrefSize(200, 30);
-        requestServiceBtn.setLayoutX(37.5);
-        requestServiceBtn.setLayoutY(250);
 
-        Button myServicesBtn = new Button("خدماتي");
-        myServicesBtn.setPrefSize(200, 30);
-        myServicesBtn.setLayoutX(37.5);
-        myServicesBtn.setLayoutY(300);
+        Button homeBtn = rightSideBarButton("الصفحة الرئيسية",37.5,200);
 
-        Button profileBtn = new Button("ملفي الشخصي");
-        profileBtn.setPrefSize(200, 30);
-        profileBtn.setLayoutX(37.5);
-        profileBtn.setLayoutY(350);
+        Button requestServiceBtn = rightSideBarButton("طلب خدمة",37.5,250);
 
-        Button settingsBtn = new Button("إعدادات");
-        settingsBtn.setPrefSize(200, 30);
-        settingsBtn.setLayoutX(37.5);
-        settingsBtn.setLayoutY(400);
+        Button myServicesBtn = rightSideBarButton("خدماتي",37.5,300);
+
+        Button profileBtn = rightSideBarButton("ملفي الشخصي",37.5,350);
+
+        Button settingsBtn = rightSideBarButton("إعدادات",37.5,400);
 
         Button logoutBtn = new Button("تسجيل الخروج");
         logoutBtn.setPrefSize(100, 30);
         logoutBtn.setLayoutX(137.5);
         logoutBtn.setLayoutY(690);
 
-        rightSideBar.getChildren().addAll(logo, homeBtn, requestServiceBtn, myServicesBtn, profileBtn, settingsBtn, logoutBtn);
+        rightSideBar.getChildren().addAll(logoImageView, homeBtn, requestServiceBtn, myServicesBtn, profileBtn, settingsBtn, logoutBtn);
 
-        Pane mainLayout = new Pane(rightSideBar,electricmenService,myRate,previosService,nowService,goToProfile,logo2,serviceSearch,trendService,woodmenService,watermenService,ironmenService,electricImageView,woodmenImageView,waterImageView,ironmenImageView,requestButton(120,200),requestButton(120,330),requestButton(120,470),requestButton(120,610),line);
+        Pane mainLayout = new Pane(rightSideBar,electricmenService,myRate,previosService,nowService,goToProfile,logoImageView2,serviceSearch,trendService,woodmenService,watermenService,ironmenService,electricImageView,woodmenImageView,waterImageView,ironmenImageView,requestButton(140,200),requestButton(120,330),requestButton(120,470),requestButton(120,610),line);
 
+        mainLayout.setStyle("-fx-background-color: #ffffff;");
         Scene scene = new Scene(mainLayout, 1200, 780);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/style.css")).toExternalForm());
 
