75be66e651f90bb2b4c6a95cf55329a58007734b
ibraheem <ebrahemebrahem401@gmail.com>
2024-08-14 00:21:32 +0300

    big fixes in PersonalHomePage.java

--- src/main/java/org/example/main/PersonalHomePage.java
+++ src/main/java/org/example/main/PersonalHomePage.java
@@ -1,10 +1,8 @@
 package org.example.main;
 
-import javafx.animation.RotateTransition;
 import javafx.animation.ScaleTransition;
 import javafx.application.Application;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -12,6 +10,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
@@ -23,128 +22,33 @@ import java.util.Objects;
 
 public class PersonalHomePage extends Application {
 
-    public static Pane rightSideBar;
+    private static final String RESOURCES_PATH = "/Pictures/";
+    private static final String CSS_PATH = "/styles/style.css";
 
-    public static void rightSideBar() {
-        rightSideBar = new Pane();
+    private Pane createRightSideBar() {
+        Pane rightSideBar = new Pane();
         rightSideBar.setId("rightSideBar");
-        rightSideBar.setPrefWidth(275);
-        rightSideBar.setPrefHeight(784);
+        rightSideBar.setPrefSize(275,784);
         rightSideBar.setLayoutX(927);
         rightSideBar.setLayoutY(-1);
 
-    }
-    public static Button requestButton(double x, double y) {
-        Button requestButton = new Button("أطلب الاَن");
-        requestButton.setId("requestButton");
-        requestButton.setPrefSize(130,110);
-        requestButton.setLayoutX(x);
-        requestButton.setLayoutY(y);
-        DropShadow shadow = new DropShadow();
-        shadow.setRadius(4.0);
-        shadow.setOffsetX(4.0);
-        shadow.setOffsetY(4.0);
-        shadow.setColor(Color.rgb(255, 255, 255, 0.5));
+        Button homeBtn = createRightSideBarButton("الصفحة الرئيسية", 200);
+        Button myServicesBtn = createRightSideBarButton("خدماتي",  300);
+        Button profileBtn = createRightSideBarButton("ملفي الشخصي",  350);
+        Button settingsBtn = createRightSideBarButton("إعدادات",  400);
 
-        requestButton.setEffect(shadow);
+        Button requestServiceBtn = new Button("طلب خدمة");
+        requestServiceBtn.setId("requestServiceBtn");
+        requestServiceBtn.setPrefSize(200, 30);
+        requestServiceBtn.setLayoutX(37.5);
+        requestServiceBtn.setLayoutY(250);
 
-        // تأثير التكبير والدوران عند التمرير
-        requestButton.setOnMouseEntered(event -> {
-            ScaleTransition scaleUp = new ScaleTransition(Duration.millis(30), requestButton);
-            scaleUp.setToX(0.9);
-            scaleUp.setToY(0.9);
+        Button logoutBtn = new Button("تسجيل الخروج");
+        logoutBtn.setPrefSize(100, 30);
+        logoutBtn.setLayoutX(137.5);
+        logoutBtn.setLayoutY(690);
 
-            scaleUp.play();
-
-        });
-        requestButton.setOnMouseExited(event -> {
-            ScaleTransition scaleDown = new ScaleTransition(Duration.millis(30), requestButton);
-            scaleDown.setToX(1);
-            scaleDown.setToY(1);
-            scaleDown.play();
-
-        });
-        return requestButton;
-    }
-    public static Button rightSideBarButton(String name,double x, double y) {
-        Button rightSideBarButton = new Button(name);
-        rightSideBarButton.setId("rightSideBarButton");
-        rightSideBarButton.setPrefSize(200,30);
-        rightSideBarButton.setLayoutX(x);
-        rightSideBarButton.setLayoutY(y);
-        return rightSideBarButton;
-    }
-
-    public static Pane servicesPane(String title, String price, double x, double y) {
-        Pane pane = new Pane();
-        pane.setPrefSize(400, 110);
-        pane.setLayoutX(x);
-        pane.setLayoutY(y);
-        pane.setStyle("-fx-background-color: #ffffff; -fx-border-color: #d2d2d2;  -fx-border-radius: 10px; -fx-background-radius: 10");
-
-        Label titleLabel = new Label(title);
-        titleLabel.setId("titleLabel");
-        titleLabel.setTextAlignment(TextAlignment.RIGHT);
-
-
-
-        titleLabel.setLayoutX(340);
-        titleLabel.setLayoutY(10);
-
-        Label priceLabel = new Label(price);
-        priceLabel.setId("priceLabel");
-
-        priceLabel.setLayoutX(360);
-        priceLabel.setLayoutY(40);
-
-
-
-
-
-
-
-        pane.getChildren().addAll(titleLabel, priceLabel);
-
-        return pane;
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        rightSideBar();
-
-        Image electric = new Image(getClass().getResource("/Pictures/electricmen.png").toExternalForm());
-        Image tailor = new Image(getClass().getResource("/Pictures/tailor.png").toExternalForm());
-        Image water = new Image(getClass().getResource("/Pictures/watermen.png").toExternalForm());
-        Image woodmen  = new Image(getClass().getResource("/Pictures/wood.png").toExternalForm());
-        Image logo1 = new Image(getClass().getResource("/Pictures/logo1.png").toExternalForm());
-        Image logo2 = new Image(getClass().getResource("/Pictures/logo2.png").toExternalForm());
-        Image accountPhoto = new Image(getClass().getResource("/Pictures/me.png").toExternalForm());
-
-
-
-        ImageView electricImageView = new ImageView(electric);
-        electricImageView.setFitHeight(110);
-        electricImageView.setFitWidth(110);
-        electricImageView.setLayoutX(650);
-        electricImageView.setLayoutY(200);
-
-        ImageView tailorImageView = new ImageView(tailor);
-        tailorImageView.setFitHeight(110);
-        tailorImageView.setFitWidth(110);
-        tailorImageView.setLayoutX(650);
-        tailorImageView.setLayoutY(330);
-
-        ImageView waterImageView = new ImageView(water);
-        waterImageView.setFitHeight(110);
-        waterImageView.setFitWidth(110);
-        waterImageView.setLayoutX(650);
-        waterImageView.setLayoutY(470);
-
-        ImageView woodmenImageView = new ImageView(woodmen);
-        woodmenImageView.setFitHeight(110);
-        woodmenImageView.setFitWidth(110);
-        woodmenImageView.setLayoutX(650);
-        woodmenImageView.setLayoutY(610);
+        Image logo1 = loadImage("logo1.png");
 
         ImageView logoImageView = new ImageView(logo1);
         logoImageView.setFitHeight(250);
@@ -152,30 +56,93 @@ public class PersonalHomePage extends Application {
         logoImageView.setLayoutX(12);
         logoImageView.setLayoutY(-40);
 
-        ImageView logoImageView2 = new ImageView(logo2);
-        logoImageView2.setFitHeight(250);
-        logoImageView2.setFitWidth(250);
-        logoImageView2.setLayoutX(-50);
-        logoImageView2.setLayoutY(-60);
+        rightSideBar.getChildren().addAll(homeBtn, myServicesBtn, profileBtn, settingsBtn, logoutBtn, requestServiceBtn, logoImageView);
 
-        ImageView accountPhotoImageView = new ImageView(accountPhoto);
-        accountPhotoImageView.setFitHeight(40);
-        accountPhotoImageView.setFitWidth(40);
-        accountPhotoImageView.setStyle("-fx-arc-height: 10;-fx-arc-width: 10");
+        return rightSideBar;
+    }
 
+    private Button createRequestButton() {
+        Button requestButton = new Button("أطلب الاَن");
+        requestButton.setId("requestButton");
+        requestButton.setPrefSize(130, 110);
 
+        DropShadow shadow = new DropShadow(4.0, 4.0, 4.0, Color.rgb(255, 255, 255, 0.5));
+        requestButton.setEffect(shadow);
 
+        requestButton.setOnMouseEntered(_ -> {
+            ScaleTransition scaleUp = new ScaleTransition(Duration.millis(30), requestButton);
+            scaleUp.setToX(0.9);
+            scaleUp.setToY(0.9);
+            scaleUp.play();
+        });
 
-        Pane electricmenService = servicesPane("كهربائي", "15$", 250, 200);
-        Pane woodmenService = servicesPane("سبّاك", "15$", 250, 470);
-        Pane tailormenService = servicesPane("خياط", "15$", 250, 330);
-        Pane watermenService = servicesPane("نجار", "15$", 250, 610);
+        requestButton.setOnMouseExited(_ -> {
+            ScaleTransition scaleDown = new ScaleTransition(Duration.millis(30), requestButton);
+            scaleDown.setToX(1);
+            scaleDown.setToY(1);
+            scaleDown.play();
+        });
 
-        Line line = new Line();
-        line.setStartX(100);
-        line.setEndX(780);
-        line.setStartY(180);
-        line.setEndY(180);
+        return requestButton;
+    }
+
+    private Button createRightSideBarButton(String name, double y) {
+        Button rightSideBarButton = new Button(name);
+        rightSideBarButton.setId("rightSideBarButton");
+        rightSideBarButton.setPrefSize(200, 30);
+        rightSideBarButton.setLayoutX(37.5);
+        rightSideBarButton.setLayoutY(y);
+        return rightSideBarButton;
+    }
+
+    private Pane createServicesPane(String title, String price, Image image, double y) {
+        Pane pane = new Pane();
+        pane.setPrefSize(680, 110);
+        pane.setLayoutX(120);
+        pane.setLayoutY(y);
+        pane.setStyle("-fx-background-color: #ffffff; -fx-border-color: #d2d2d2; -fx-border-radius: 10px; -fx-background-radius: 10");
+
+        Label titleLabel = new Label(title);
+        titleLabel.setId("titleLabel");
+        titleLabel.setPrefWidth(300); // تحديد عرض ثابت للعنوان لتسهيل المحاذاة
+        titleLabel.setLayoutX(250);
+        titleLabel.setLayoutY(10);
+        titleLabel.setAlignment(Pos.CENTER_RIGHT); // محاذاة النص إلى اليمين
+        titleLabel.setTextAlignment(TextAlignment.RIGHT); // محاذاة النص داخل الـ Label
+
+        Label priceLabel = new Label(price);
+        priceLabel.setId("priceLabel");
+        priceLabel.setLayoutX(520);
+        priceLabel.setLayoutY(40);
+
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(110);
+        imageView.setFitWidth(110);
+        imageView.setLayoutX(578);
+        imageView.setLayoutY(0);
+
+        Button requestButton = createRequestButton();
+
+        pane.getChildren().addAll(titleLabel, priceLabel, imageView, requestButton);
+        return pane;
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        Pane rightSideBar = createRightSideBar();
+
+        Image electric = loadImage("worker.png");
+        Image tailor = loadImage("worker.png");
+        Image water = loadImage("worker.png");
+        Image woodmen = loadImage("worker.png");
+        Image logo2 = loadImage("logo2.png");
+        Image accountPhoto = loadImage("me.png");
+
+        Pane electricPane = createServicesPane("كهربائي", "15$", electric, 200);
+        Pane waterPane = createServicesPane("سبّاك", "30$", water,  470);
+        Pane tailorPane = createServicesPane("خياط", "45$", tailor,  330);
+        Pane woodmenPane = createServicesPane("نجار", "28$", woodmen, 610);
+
+        Line line = new Line(100, 180, 840, 180);
         line.setStroke(Color.web("d2d2d2"));
 
         Label trendService = new Label("الخدمات الشائعة");
@@ -185,76 +152,59 @@ public class PersonalHomePage extends Application {
 
         TextField serviceSearch = new TextField("البحث عن خدمة...");
         serviceSearch.setId("serviceSearch");
-
         serviceSearch.setLayoutX(120);
         serviceSearch.setLayoutY(140);
-        serviceSearch.setPrefSize(200,30);
+        serviceSearch.setPrefSize(200, 30);
 
-        Button goToProfile = new Button("",accountPhotoImageView);
+        ImageView accountPhotoImageView = new ImageView(accountPhoto);
+        accountPhotoImageView.setFitHeight(40);
+        accountPhotoImageView.setFitWidth(40);
+        accountPhotoImageView.setStyle("-fx-arc-height: 10;-fx-arc-width: 10");
+
+        Button goToProfile = new Button("", accountPhotoImageView);
         goToProfile.setStyle("-fx-background-color: #ffffff; -fx-border-radius: 10px; -fx-background-radius: 10px;");
-        goToProfile.setPrefSize(70,70);
+        goToProfile.setPrefSize(70, 70);
         goToProfile.setLayoutX(120);
         goToProfile.setLayoutY(15);
 
         Button nowService = new Button("خدماتي الحالية");
-        nowService.setPrefSize(100,30);
+        nowService.setPrefSize(100, 30);
         nowService.setLayoutX(750);
         nowService.setLayoutY(10);
 
-        Button previosService = new Button("خدماتي السابقة");
-        previosService.setPrefSize(100,30);
-        previosService.setLayoutX(630);
-        previosService.setLayoutY(10);
+        Button previousService = new Button("خدماتي السابقة");
+        previousService.setPrefSize(100, 30);
+        previousService.setLayoutX(630);
+        previousService.setLayoutY(10);
 
         Button myRate = new Button("تقييماتي");
-        myRate.setPrefSize(100,30);
+        myRate.setPrefSize(100, 30);
         myRate.setLayoutX(510);
         myRate.setLayoutY(10);
 
+        ImageView logoImageView2 = new ImageView(logo2);
+        logoImageView2.setFitHeight(250);
+        logoImageView2.setFitWidth(250);
+        logoImageView2.setLayoutX(-50);
+        logoImageView2.setLayoutY(-60);
 
-
-
-
-
-
-
-
-
-
-        Button homeBtn = rightSideBarButton("الصفحة الرئيسية",37.5,200);
-
-        Button requestServiceBtn = new Button("طلب خدمة");
-        requestServiceBtn.setId("requestServiceBtn");
-        requestServiceBtn.setPrefSize(200,30);
-        requestServiceBtn.setLayoutX(37.5);
-        requestServiceBtn.setLayoutY(250);
-
-
-        Button myServicesBtn = rightSideBarButton("خدماتي",37.5,300);
-
-        Button profileBtn = rightSideBarButton("ملفي الشخصي",37.5,350);
-
-        Button settingsBtn = rightSideBarButton("إعدادات",37.5,400);
-
-        Button logoutBtn = new Button("تسجيل الخروج");
-        logoutBtn.setPrefSize(100, 30);
-        logoutBtn.setLayoutX(137.5);
-        logoutBtn.setLayoutY(690);
-
-        rightSideBar.getChildren().addAll(logoImageView, homeBtn, requestServiceBtn, myServicesBtn, profileBtn, settingsBtn, logoutBtn);
-
-        Pane mainLayout = new Pane(rightSideBar,electricmenService,myRate,previosService,nowService,goToProfile,logoImageView2,serviceSearch,trendService,woodmenService,watermenService,tailormenService,electricImageView,woodmenImageView,waterImageView,tailorImageView,requestButton(120,200),requestButton(120,330),requestButton(120,470),requestButton(120,610),line);
-
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.setPrefSize(1200,780);
         mainLayout.setStyle("-fx-background-color: #ffffff;");
-        Scene scene = new Scene(mainLayout, 1200, 780);
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/style.css")).toExternalForm());
+        mainLayout.getChildren().addAll( rightSideBar, myRate, previousService, nowService, goToProfile,
+                logoImageView2, serviceSearch, trendService, line, electricPane, waterPane, tailorPane, woodmenPane);
+        Scene scene = new Scene(mainLayout);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource(CSS_PATH)).toExternalForm());
 
         primaryStage.setTitle("Jibo");
-        primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Pictures/icon.png"))));
-
+        primaryStage.getIcons().add(loadImage("icon.png"));
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    private Image loadImage(String imageName) {
+        return new Image(Objects.requireNonNull(getClass().getResource(RESOURCES_PATH + imageName)).toExternalForm());
     }
 
     public static void main(String[] args) {
