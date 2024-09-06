5ddf2a9eb971595c627bb66fc4fd52a0c0f8d81d
Tati-I <ebrahemebrahem401@gmail.com>
2024-09-06 23:04:10 +0300

    test

--- src/main/java/auth/EmailCheck.java
+++ src/main/java/auth/EmailCheck.java
@@ -7,6 +7,7 @@ public class EmailCheck {
     // Regular expression for validating an email address
     private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
 
+
     public static boolean isValidEmail(String email) {
         if (email == null || email.isEmpty()) {
             return false;
